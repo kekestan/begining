b0c780209248499219d6a11f169dc26a9b0cebce
kekestan <44110607+kekestan@users.noreply.github.com>
2018-11-10 16:22:53 +0700

    Add files via upload

---
+++ Lesson1.py
@@ -0,0 +1,8 @@
+import requests
+print("Enter you Email or nick")
+login = input()
+print("Enter you password")
+password = input()
+r = requests.get('https://api.github.com/user', auth=(login, password))
+print(r.status_code)
+print(r.json())
